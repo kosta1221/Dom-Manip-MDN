bbeeb51f1c3e83e59cdd4660de26559effafcc0a
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-01-24 23:29:44 +0200

    Active learning: A dynamic shopping list
    Active learning: A dynamic shopping list

--- jsdommanip.js
+++ jsdommanip.js
@@ -1,13 +1,25 @@
-const div = document.querySelector("div");
-let winWidth = window.innerWidth;
-let winHeight = window.innerHeight;
+const ul = document.querySelector("ul");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
 
-div.style.width = winWidth + "px";
-div.style.height = winHeight + "px";
+button.onclick = function () {
+	let inputCurrentValue = input.value;
+	input.value = "";
 
-window.onresize = function () {
-	winWidth = window.innerWidth;
-	winHeight = window.innerHeight;
-	div.style.width = winWidth + "px";
-	div.style.height = winHeight + "px";
+	const liItem = document.createElement("li");
+	const span = document.createElement("span");
+	const myButton = document.createElement("button");
+
+	liItem.appendChild(span);
+	liItem.appendChild(myButton);
+
+	span.innerText = inputCurrentValue;
+	myButton.innerText = "Delete";
+
+	ul.appendChild(liItem);
+
+	myButton.onclick = function () {
+		ul.removeChild(liItem);
+	};
+	input.focus();
 };
