ddd381b71bc9e31891b07aa8b64330099e23b7b1
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-01-24 22:59:58 +0200

    Active learning - Getting useful information from the Window object
    Getting useful information from the Window object

--- jsdommanip.js
+++ jsdommanip.js
@@ -1,23 +1,13 @@
-const link = document.querySelector("a");
-link.textContent = "Mozilla Developer Network";
-link.href = "https://developer.mozilla.org";
+const div = document.querySelector("div");
+let winWidth = window.innerWidth;
+let winHeight = window.innerHeight;
 
-const sect = document.querySelector("section");
-const para = document.createElement("p");
-para.textContent = "We hope you enjoyed the ride.";
-sect.appendChild(para);
-const text = document.createTextNode(" — the premier source for web development knowledge.");
+div.style.width = winWidth + "px";
+div.style.height = winHeight + "px";
 
-const linkPara = document.querySelector("p");
-linkPara.appendChild(text);
-
-sect.appendChild(linkPara);
-linkPara.remove();
-
-/* para.style.color = 'white';
-para.style.backgroundColor = 'black';
-para.style.padding = '10px';
-para.style.width = '250px';
-para.style.textAlign = 'center'; */
-
-para.setAttribute("class", "highlight");
+window.onresize = function () {
+	winWidth = window.innerWidth;
+	winHeight = window.innerHeight;
+	div.style.width = winWidth + "px";
+	div.style.height = winHeight + "px";
+};
